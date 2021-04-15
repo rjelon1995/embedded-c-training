36728529f138e240c78602b1e0d7514a926c6e8c
rjelon1995 <82601532+rjelon1995@users.noreply.github.com>
2021-04-15 12:56:52 -0500

    Update hello_world.c

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include "hello_functions.h"
 
 int main(void){
-  printf("Hello World!");
- }
- 
+   printf("Hello World\n");
+   int x = 2;
+   int y = 3;
+   int z = add(x, y);
+}
